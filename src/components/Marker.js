a62225062e4057936717d9d3dd8f69f90f3170bf
Etienne <e.sharkey@live.com>
2022-09-24 19:26:53 +0100

    New popover styling

--- src/components/Marker.js
+++ src/components/Marker.js
@@ -1,7 +1,10 @@
 import React, {useState, useRef, useEffect} from 'react';
 import PropTypes from 'prop-types';
 import styled from 'styled-components';
-import Overlay from 'react-bootstrap/Overlay';
+import OverlayTrigger from 'react-bootstrap/OverlayTrigger';
+import Popover from 'react-bootstrap/Popover';
+import useWindowDimensions from './../hooks/useWindowDimensions'
+
 
 
 
@@ -41,64 +44,30 @@ function useOutsideAlerter(ref, setShow) {
 }
 
 const Marker = ({ text, onClick, photo, photoName, username, date }) => {
-  const [show, setShow] = useState(false);
-  const target = useRef(null);
-  useOutsideAlerter(target, setShow);
+  const { height, width } = useWindowDimensions();
+
 
   useEffect(()=>{
-    document.addEventListener('click', setShow(false))
   }, [])
+  
+
+  const popover = (
+    <Popover id="popover-photo">
+      <Popover.Header as="h1">{photoName}</Popover.Header>
+      <Popover.Body>
+        <p>Uploaded by {username}</p>
+        <div className='image-container'>
+          <img src={photo} className='w-100'></img>
+        </div>
+      </Popover.Body>
+    </Popover>
+  );
 
 return(
-
-  
   <>
-  <Wrapper
-    variant="danger" ref={target} onClick={() => setShow(!show)}
-    />
-      <Overlay target={target.current} show={show} placement="right" rootClose={true} rootCloseEvent={'click'}>
-        {({ placement, arrowProps, show: _show, popper, ...props }) => (
-          <div
-            {...props}
-            style={{
-              position: 'absolute',
-              backgroundColor: '#f2f2f2',
-              color: '#000',
-              borderRadius: 3,
-              border: '1px solid #000',
-              ...props.style,
-              width: '400px'
-            }}
-          >
-            <div className='row image-popUp'>
-              <div className='col-4'>
-                <div className='row'>
-                  <div className='col-12'>
-                    <h3 className='text-uppercase text-center'>{photoName}</h3>
-                  </div>
-                </div>
-                <div className='row'>
-                  <div className='col-12'>
-                    <h6>{date}</h6>
-                  </div>
-                </div>
-
-
-
-
-                <p>Uploaded by {username}</p>
-              </div>
-
-              <div className='col-8'>
-                <div className='image-container'>
-                  <img src={photo} className='float-end h-100 w-100'></img>
-                </div>
-              </div>
-            </div>
-            
-          </div>
-        )}
-      </Overlay>
+    <OverlayTrigger trigger="click" placement="top" overlay={popover} rootClose>
+      <Wrapper variant="success"></Wrapper>
+    </OverlayTrigger>
 
 
 </>
